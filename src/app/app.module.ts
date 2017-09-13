e536c4b0c4f6704b0ba8e37f152cc2825141ffe7
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-09-13 22:39:02 +0530

    Two way binding and currency pipe.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, ProductListComponent],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -7,6 +7,7 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
     private pageTitle: String = 'Product List';
+    private listFilter = '';
     private isImageDisplayed: Boolean = false;
     private products: any[] = [
         {
